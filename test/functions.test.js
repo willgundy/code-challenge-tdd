283f5a4f71504e008e6cb064a7e9857e6d451b06
Will Gundy <willgunderson503@gmail.com>
2022-03-10 11:03:24 -0800

    Add test cases for exclamation points function

--- test/functions.test.js
+++ test/functions.test.js
@@ -32,3 +32,23 @@ skip('this test should be skipped', (expect) => {
     expect.equal(actual, expected);
 });
 
+test('addExclamationPoints() should add three exclamation points to a string', (expect) => {
+    const expected = 'bunny rabbit!!!';
+
+    const actual = addExclamationPoints('bunny rabbit');
+
+    expect.equal(actual, expected, 'Should return !!! after bunny rabbit');
+
+    const expected2 = 'hooray!!!';
+
+    const actual2 = addExclamationPoints('hooray');
+
+    expect.equal(actual2, expected2, 'Should return !!! after hooray');
+
+    const expected3 = 'ice cream!!!';
+
+    const actual3 = addExclamationPoints('ice cream');
+
+    expect.equal(actual3, expected3, 'Should return !!! after ice cream');
+});
+
